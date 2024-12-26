ed06e78d513d69119b52affdcc6762c0d9389a28
loganphillips792 <loganphillips792@gmail.com>
2024-12-26 02:29:25 -0500

    wip

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,21 +1,30 @@
 return{
   {
-      "nvim-neo-tree/neo-tree.nvim",
-      branch = "v3.x",
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-      },
-      config = function()
-        -- Automatically open neo-tree on startup
-	vim.cmd([[Neotree show]])
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+      -- Automatically open neo-tree on startup
+      vim.cmd([[Neotree show]])
 
-        require("neo-tree").setup({
-		close_if_last_window = true,
-          	enable_git_status=true,
-        })
-      end
+      require("neo-tree").setup({
+        close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+        enable_git_status=true,
+        default_component_configs = {
+          indent = {
+            indent_size = 2,
+            padding = 1,
+            with_markers = true,
+            indent_marker = "|",
+            highlight = "NeoTreeIndentMarker",
+          },
+        }
+      })
+    end
   }
 }
