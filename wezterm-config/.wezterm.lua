28af329086957b08aec884687ced3d2c27012868
loganphillips792 <loganphillips792@gmail.com>
2025-01-06 16:40:21 -0600

    set theme for wezterm

--- wezterm-config/.wezterm.lua
+++ wezterm-config/.wezterm.lua
@@ -1,13 +1,31 @@
 local wezterm = require("wezterm")
 
 local function set_size_of_emulator()
- return {
-  initial_rows = 20,
-  initial_cols = 120,
-  font_size = 15.0,
-}
+  return {
+    initial_rows = 20,
+    initial_cols = 120,
+    font_size = 15.0,
+  }
 end
 
+local function set_theme()  
+  return {
+      color_scheme = "Dracula (Official)",
+      tab_bar_at_bottom = true,
+      use_fancy_tab_bar = false,
+      window_decorations = "RESIZE"
+  }
+end
+
+-- Merge the two configurations
+local function merge_tables(tbl1, tbl2)
+  for k, v in pairs(tbl2) do
+    tbl1[k] = v
+  end
+  return tbl1
+end
+
+
 -- configuration file expects a table to be returned directly
-return set_size_of_emulator()
+return merge_tables(set_size_of_emulator(), set_theme())
 
