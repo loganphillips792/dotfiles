fa92d9dafd1612cd3fa568fb9bb0747e03330deb
loganphillips792 <loganphillips792@gmail.com>
2025-01-06 18:03:02 -0600

    change color scheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -8,7 +8,7 @@ return {
       vim.cmd([[colorscheme tokyonight]])
     end
   } --]=]
-  
+
   --[=[
   {
     "franbach/miramare",
@@ -18,7 +18,7 @@ return {
   }
   --]=]
 
-  {
+   --[=[{
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
@@ -26,5 +26,28 @@ return {
       vim.cmd([[colorscheme catppuccin-macchiato]])
     end
   }
-
+  --]=]
+  
+  "sainnhe/gruvbox-material",
+  priority = 1000,
+  config = function()
+    vim.o.background = "dark" -- or "light" for light mode
+    vim.cmd("let g:gruvbox_material_background= 'hard'")
+    vim.cmd("let g:gruvbox_material_transparent_background=2")
+    vim.cmd("let g:gruvbox_material_diagnostic_line_highlight=1")
+    vim.cmd("let g:gruvbox_material_diagnostic_virtual_text='colored'")
+    vim.cmd("let g:gruvbox_material_enable_bold=1")
+    vim.cmd("let g:gruvbox_material_enable_italic=1")
+    vim.cmd([[colorscheme gruvbox-material]]) -- Set color scheme
+    -- changing bg and border colors
+    vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
+    vim.api.nvim_set_hl(0, "LspInfoBorder", { link = "Normal" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
+    vim.api.nvim_set_hl(0, "Pmenu", { link = "Normal" })
+    vim.api.nvim_set_hl(0, "PmenuSel", { link = "Search" })
+    vim.api.nvim_set_hl(0, "BlinkCmpMenu", { link = "Normal" })
+    vim.api.nvim_set_hl(0, "BlinkCmpMenuBorder", { link = "Normal" })
+    vim.api.nvim_set_hl(0, "BlinkCmpMenuSelection", { link = "Search" })
+    vim.api.nvim_set_hl(0, "BlinkCmpLabelMatch", { link = "Search" })
+  end,
 }
